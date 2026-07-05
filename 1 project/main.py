fe5de71c980c6abf3cf76c5dabf3af226abd59fb
Ilya <ilj_1702@mail.ru>
2026-07-06 00:01:10 +0300

    1 project 2 version

--- 1 project/main.py
+++ 1 project/main.py
@@ -13,7 +13,8 @@ def update_tasks(data, file_name):
     with open(file_name, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
-def add_task(title, tasks):
+def add_task(title, file_name):
+    tasks = read_tasks(file_name)
     title = title.split(", ")
 
     if tasks["tasks"]:
@@ -29,17 +30,64 @@ def add_task(title, tasks):
         "tags": task_tags,
         "done": False
     })
+    update_tasks(tasks, file_name)
 
-def list_tasks(tasks, filter_tag=None):
+
+def list_tasks(file_name, filter_tag=None):
+    tasks = read_tasks(file_name)
     print("|id|title|tags|done")
     for task in tasks["tasks"]:
         print(f"| {task['id']} | {task['title']} | {task['tags']} | {task['done']} |")
 
+def done_task(file_name, task_id):
+    tasks = read_tasks(file_name)
+    for task in tasks["tasks"]:
+        if task['id'] == task_id:
+            task['done'] = True
+            break
+    update_tasks(tasks, file_name)
+
+def delete_task(file_name, title):
+    tasks = read_tasks(file_name)
+    for task in tasks["tasks"]:
+        if task['id'] == task_id:
+            tasks["tasks"].remove(task)
+            break
+    update_tasks(tasks, file_name)
+
+def clean(file_name):
+    data = {'tasks':[]}
+    update_tasks(data, file_name)
+
 file_name = "tasks.json"
 
-data = read_tasks(file_name)
-print(data)
-new_task = input("Введи новою задачу: ")
-add_task(new_task, data)
-update_tasks(data, file_name)
-list_tasks(data)
+while True:
+    print("Есть набор команд:" \
+    "\n 1. Добавить дело - add" \
+    "\n 2. Удалить дело по названию - delete" \
+    "\n 3. Отметить дело сделанным - done" \
+    "\n 4. Отчистить список - clean" \
+    "\n 5. Вывести список - list")
+
+    command = input("Введи команду: ").strip().lower()
+        
+    match command:
+        case "list":
+            list_tasks(file_name)
+        case "add":
+            title = input("Введи новую задачу: ")
+            add_task(title, file_name)
+        case "done":
+            task_id = int(input("Введи ID задачи: "))
+            done_task(file_name, task_id)
+        case "delete":
+            task_id = int(input("Введи ID задачи: "))
+            delete_task(file_name, task_id)
+        case "clean":
+            clean(file_name)
+            print("Выполненные задачи удалены!")
+        case "exit":
+            print("До свидания!")
+            break
+        case _:
+            print("Неизвестная команда!")
